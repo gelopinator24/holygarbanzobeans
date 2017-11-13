ef22dc50fdda6f38de5c1a6b9db52f6fb0bbf3ef
Roshan Haque <roshan.haque@gmail.com>
2017-11-13 21:54:59 +0000

    Help me

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -2,7 +2,12 @@ function sayThatWasEasy() {
   var thatWasEasy = new Audio("EasyWeird.mp3");
   thatWasEasy.play();
 }
+function sayThatWasHard() {
+  var thatWasEasy = new Audio("boos3.mp3");
+  thatWasEasy.play();
+}
 $("#easy").on("click", sayThatWasEasy);
+$("#hard").on("click", sayThatWasHard);
 $(document).keypress(delegateKeypress);
 function delegateKeypress(event) {
   if (event.charCode == 32) {
